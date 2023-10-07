e89649d64a0fb299cd2247d855f43fd0f5b87ddb
harmonia2side <breno_odnan@hotmail.com>
2023-10-07 17:52:11 -0300

    adiciona depedências da aula

---
+++ aula4.sh
@@ -0,0 +1,20 @@
+# Comandos da aula 4 - Setup do ambiente
+
+# Instala dependências do curso
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git curl build-essential -y
+sudo apt install gcc make default-libmysqlclient-dev libssl-dev -y
+# Instala python mais atualizado
+sudo apt install python3.10-full python3.10-dev -y
+
+# Cria ambiente virtual
+python3 -m venv .venv
+python --version
+# Observe que ele está usando o python específico do ambiente
+which python
+
+# Atualiza o Pip
+python -m pip install pip
+
+
